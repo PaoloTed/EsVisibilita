2700dffb05ef50bea5d557e39f0f42d7d85d579d
Paolo Tedesco <paol.tedesco@studenti.unina.it>
2023-03-31 11:23:12 +0200

    commit 1

---
+++ src/main/java/org/example/A.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public class A {
+    protected int val = 5;
+}
---
+++ src/main/java/org/example/B.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class B extends A{
+    public void f(){
+        System.out.println(val);
+    }
+}
